90db768e608f903e002e748a6e47487928741e09
Connor Christie <chillercraft@hotmail.com>
2020-03-01 23:14:56 -0600

    Working coravel pro project

---
+++ VirtualAdmin/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Coravel.Pro.EntityFramework;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VirtualAdmin2
+{
+    public class ApplicationDbContext : IdentityDbContext, ICoravelProDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<CoravelJobHistory> Coravel_JobHistory { get; set; }
+        public DbSet<CoravelScheduledJob> Coravel_ScheduledJobs { get; set; }
+        public DbSet<CoravelScheduledJobHistory> Coravel_ScheduledJobHistory { get; set; }
+    }
+}
